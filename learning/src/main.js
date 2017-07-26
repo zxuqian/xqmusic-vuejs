43bfe9e827d46e933b2031c84e59a1a647e733db
Xuqian <zxuqian@gmail.com>
2017-07-26 11:27:32 -0400

    a few examples

--- learning/src/main.js
+++ learning/src/main.js
@@ -5,11 +5,41 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello World'
+    message: 'Hello World',
+    seen: true,
+    notSeen: false,
+    todos: [
+      { id: 1, text: 'Todo item1' },
+      { id: 2, text: 'Todo item2' },
+      { id: 3, text: 'Todo item3' }
+    ]
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('')
+    }
+  },
+  filters: {
+    capitalize: function(value) {
+      if(!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+
+    }
+  },
+  computed: {
+    computedMessage: function() {
+      return this.message + " computed!"
+    }
   }
   //template: '<App/>',
   //components: { App }
